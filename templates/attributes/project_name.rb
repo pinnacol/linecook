5963c26e6b5a04112ff2c8b32fe2ce4f7040245a
Simon Chiang <simon.chiang@pinnacol.com>
2010-12-20 15:11:31 -0700

    made bin/line_cook use erb

--- templates/attributes/project_name.rb
+++ templates/attributes/project_name.rb
@@ -1,4 +1,4 @@
 # Define default recipe attributes here and include in a recipe using
 # 'attributes "filename"'. Script attributes override these values.
-attrs['%project_name%']['letters'] = ['a', 'b', 'c']
-attrs['%project_name%']['numbers'] = [1, 2, 3]
+attrs['<%= project_name %>']['letters'] = ['a', 'b', 'c']
+attrs['<%= project_name %>']['numbers'] = [1, 2, 3]
--- templates/recipes/project_name.rb
+++ templates/recipes/project_name.rb
@@ -1,10 +1,10 @@
 # Include any helpers/attributes used by this recipe
-helpers '%project_name%'
-attributes '%project_name%'
+helpers '<%= project_name %>'
+attributes '<%= project_name %>'
 
 # Write to the script target using 'script'
 script.puts '# An example script.'
 
 # Helpers are now available, as are attributes.
-echo *attrs['%project_name%']['letters']
-echo *attrs['%project_name%']['numbers']
+echo *attrs['<%= project_name %>']['letters']
+echo *attrs['<%= project_name %>']['numbers']
