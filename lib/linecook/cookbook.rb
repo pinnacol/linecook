b09332b273de877c648fc95fee29800e9457128e
Simon Chiang <simon.chiang@pinnacol.com>
2010-12-22 11:40:14 -0700

    made linecook use linebook

--- lib/linecook/cookbook.rb
+++ lib/linecook/cookbook.rb
@@ -1,89 +1,52 @@
+require 'linebook'
 require 'linecook/utils'
 
 module Linecook
   class Cookbook
     class << self
-      def split_path(path)
-        path.kind_of?(String) ? path.split(':') : path
+      def config_file(dir)
+        Dir.glob(File.join(dir, '{Ll}inebook')).first
       end
-      
-      def cookbook_file(dir)
-        File.expand_path('Cookbook', dir)
-      end
-      
-      def init_from(dir)
-        file  = cookbook_file(pwd)
-        attrs = File.exists?(file) ? YAML.load_file(file) : nil
-        init(attrs || {})
-      end
-      
-      def init(attrs)
-        require 'rubygems' unless Object.const_defined?(:Gem)
         
-        path = attrs['path'] || '.'
-        dirs = split_path(path)
-        
-        gems = attrs['gems'] || Gem.loaded_specs.keys
-        gems.each do |gem_name|
-          if spec = Gem.source_index.find_name(gem_name).last
-            dir  = spec.full_gem_path
-            dirs << dir if File.exists?(cookbook_file(dir))
-          end
-        end
-        
-        new(*dirs)
-      end
-    end
-    include Utils
-    
-    FILE_PATTERNS = {
-      :attributes  => File.join('attributes', '**', '*.rb'),
-      :files       => File.join('files', '**', '*'),
-      :helpers     => File.join('helpers', '**', '*.rb'),
-      :recipes     => File.join('recipes', '**', '*.rb'),
-      :templates   => File.join('templates', '**', '*.erb')
-    }
-    
-    attr_reader :dirs
-    attr_reader :files
-    
-    def initialize(*dirs)
-      @dirs  = dirs.collect {|dir| File.expand_path(dir) }
-      @files = Hash.new do |hash, key| 
-        pattern = FILE_PATTERNS[key]
-        hash[key] = pattern ? glob(pattern) : nil
+      def init(dir)
+        path   = config_file(dir)
+        config = path ? YAML.load_file(path) : nil
+        new(config || {})
       end
       
-      @manifest = nil
-    end
-    
-    def [](type)
-      files[type]
-    end
-    
-    def glob(pattern)
-      files = {}
-      
-      dirs.each do |dir|
-        Dir.glob(File.join(dir, pattern)).each do |path|
-          next unless File.file?(path)
-          
-          relative_path = path[(dir.length+1)..-1]
-          files[relative_path] ||= path
+      def gems
+        return [] unless Object.const_defined?(:Gem)
+        
+        Gem.source_index.latest_specs.select do |spec|
+          config_file(spec.full_gem_path) != nil
+        end.collect do |spec|
+          spec.name
         end
       end
-      
-      files
+    end
+    
+    include Linebook
+    
+    PATTERNS = [
+      ['', File.join('attributes', '**', '*.rb')],
+      ['', File.join('files',      '**', '*')],
+      ['', File.join('helpers',    '**', '*.rb')],
+      ['', File.join('recipes',    '**', '*.rb')],
+      ['', File.join('templates',  '**', '*.erb')]
+    ]
+    
+    attr_reader :config
+    
+    def initialize(config)
+      @config = {
+         'patterns' => PATTERNS,
+         'paths' => ['.'],
+         'gems'  => self.class.gems
+       }.merge(config)
     end
     
     def manifest
-      @manifest ||= begin
-        manifest = {}
-        FILE_PATTERNS.each_key do |type|
-          manifest.merge! files[type]
-        end
-        manifest
-      end
+      @manifest ||= __manifest(config)
     end
   end
 end
