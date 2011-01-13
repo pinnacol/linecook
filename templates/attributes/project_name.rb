90a072ec0d2ed994b6950937213594970a9ad735
Simon Chiang <simon.chiang@pinnacol.com>
2011-01-13 14:19:09 -0700

    added comment about using strings rather than symbols

--- templates/attributes/project_name.rb
+++ templates/attributes/project_name.rb
@@ -1,3 +1,4 @@
 # Define default recipe attributes here.
+# (note string keys are usually better than symbols)
 attrs['<%= project_name %>']['letters'] = ['a', 'b', 'c']
 attrs['<%= project_name %>']['numbers'] = [1, 2, 3]
