5878e0753100c6f4f0fa06a9d4bb9588773ba755
Simon Chiang <simon.chiang@pinnacol.com>
2011-01-12 17:16:50 -0700

    added preview command

---
+++ lib/linecook/commands/preview.rb
@@ -0,0 +1,40 @@
+require 'linecook/commands/command'
+require 'linecook/cookbook'
+require 'linecook/package'
+require 'yaml'
+
+module Linecook
+  module Commands
+    
+    # ::desc generates a package
+    #
+    # Generates a package.
+    #
+    class Preview < Command
+      config :cookbook_dir, '.', :short => :d       # the cookbook directory
+      config :force, false, :short => :f, &c.flag   # force creation
+      
+      def process(recipe_name, source=nil)
+        cookbook = Linecook::Cookbook.init(cookbook_dir)
+        package  = Linecook::Package.load(source, cookbook)
+        
+        package.recipes.clear
+        package.recipes[recipe_name] = recipe_name
+        
+        package.build
+        display(package, recipe_name)
+        
+        package.registry.keys.sort.each do |name|
+          next if name == recipe_name
+          display(package, name)
+        end
+      end
+      
+      def display(package, name)
+        puts "#" * 78
+        puts "[#{name}] "
+        puts package.content(name)
+      end
+    end
+  end
+end
