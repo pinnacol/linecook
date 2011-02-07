bbaa065f3b0081a5980abd34bfd1779fd3b98c95
Simon Chiang <simon.chiang@pinnacol.com>
2011-02-07 10:30:22 -0700

    documentation of preview command

--- lib/linecook/commands/preview.rb
+++ lib/linecook/commands/preview.rb
@@ -7,23 +7,34 @@ module Linecook
   module Commands
     
     # ::desc preview recipe output
+    #
+    # Evaluates and prints the output of a recipe (ie the resulting script and
+    # any associated files). Filters can be applied to examine only parts of
+    # the recipe output, for example only files matching a specific pattern:
+    #
+    #   % linecook preview recipe_name --only pattern
+    #
+    # By default .zip and .gz files are filtered, both because they are
+    # typically big and because they don't print as plain text.
     class Preview < Command
-      config :cookbook_dir, '.', :short => :d       # the cookbook directory
-      config :force, false, :short => :f, &c.flag   # force creation
-      config :source, nil                           # the package file
+      config :project_dir, '.', :short => :d        # the project directory
+      config :package_path, nil, :short => :p       # the package path
+      
       config :only, '.' do |filter|                 # an 'only' target filter
         Regexp.new(filter)
       end
-      config :except, '\.zip$' do |filter|          # an 'except' target filter
+      
+      config :except, '\.(zip|gz)$' do |filter|     # an 'except' target filter
         Regexp.new(filter)
       end
-      config :max, 10 ** 4, &c.integer              # max length to read
+      
+      config :max, 10 ** 4, &c.integer              # max length to display (per file)
       
       def process(*recipes)
-        cookbook = Linecook::Cookbook.init(cookbook_dir)
-        package  = Linecook::Package.load(source, cookbook)
-
-        package.config['recipes'] = recipes
+        cookbook = Linecook::Cookbook.init(project_dir)
+        package  = Linecook::Package.load(package_path, cookbook)
+        
+        package.config[Linecook::Package::RECIPES_KEY] = recipes
         package.build
 
         package.registry.keys.sort.each do |name|
