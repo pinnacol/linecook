a4383bcafdbaacc5f32ef99d52377096cef3504d
Simon Chiang <simon.chiang@pinnacol.com>
2011-02-17 16:19:16 -0700

    sketched out a run/scp script pair allowing run with args

--- lib/linecook/commands/run.rb
+++ lib/linecook/commands/run.rb
@@ -7,78 +7,48 @@ module Linecook
     # ::desc 
     class Run < Command
       RUN_SCRIPT = File.expand_path('../../../../bin/linecook_run', __FILE__)
+      SCP_SCRIPT = File.expand_path('../../../../bin/linecook_scp', __FILE__)
       
       config :project_dir, '.', :short => :d                 # the project directory
-      config :remote_dir, 'vm', :short => :D
+      config :remote_dir, 'vm/pkg', :short => :D
       config :ssh_config_file, 'config/ssh', :short => :F
       config :quiet, false, :short => :q, &c.flag            # silence output
-      config :verbose, false, :short => :v, &c.flag          # verbose output
-      config :xtrace, false, :short => :x, &c.flag           # xtrace output
+      config :transfer, true, &c.switch
       config :file, false, &c.flag                           # treat package name as file path
-      config :run_script, 'run', :short => :r
-      config :test_script, 'test', :short => :t
-      
-      def xtrace?
-        xtrace
-      end
-      
-      def verbose?
-        verbose && !xtrace?
-      end
-      
-      def quiet?
-        quiet && !verbose?
-      end
+      config :cmd_file, nil
       
       def process(*package_names)
-        shell_opts = {
-          'v' => verbose?,
-          'x' => xtrace?
-        }
-        
-        run_opts = {
-          'D' => remote_dir,
-          's' => ssh_opts,
-          'c' => scp_opts,
-          'F' => ssh_config_file,
-          'r' => run_script,
-          't' => test_script
-        }
-        
         package_names = glob_package_names(project_dir) if package_names.empty?
         package_dirs  = collect_package_dirs(package_names)
         
-        cmd = ["sh", format(shell_opts), RUN_SCRIPT, format(run_opts)] + package_dirs
-        cmd.delete_if {|arg| arg.empty? }
+        opts = {
+          'D' => remote_dir,
+          'F' => ssh_config_file
+        }
         
-        sh! cmd.join(' ')
+        sh! "sh #{SCP_SCRIPT} #{format(opts)} #{package_dirs.join(' ')}" if transfer
+        sh! "sh #{RUN_SCRIPT} #{format(opts)} #{package_dirs.join(' ')} #{source(cmd_file)}"
+      end
+      
+      def source(cmd_file)
+        case cmd_file
+        when nil
+          "<<DOC\nrun\ntest\nDOC"
+        when '-'
+          ""
+        else
+          "< '#{cmd_file}'"
+        end
       end
       
       def format(opts)
         options = []
         
-        opts.keys.sort.collect do |key|
-          value = opts[key]
-          
-          case value
-          when true
-            options << "-#{key}"
-          when false
-            options << "+#{key}"
-          else
-            options << "-#{key} '#{value}'"
-          end
+        opts.each_pair do |key, value|
+          options << "-#{key} '#{value}'"
         end
         
-        options.join(' ')
-      end
-      
-      def ssh_opts
-        quiet? ? '-q -T' : ''
-      end
-      
-      def scp_opts
-        quiet? ? '-q' : ''
+        options.sort.join(' ')
       end
       
       def glob_package_names(project_dir)
