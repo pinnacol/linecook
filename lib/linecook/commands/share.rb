9f856568c54e27283a24577228fab5d8e35a0f77
Simon Chiang <simon.chiang@pinnacol.com>
2011-02-18 16:48:48 -0700

    remove virtualbox shares and updated setup instructions to reflect use of sshfs

--- lib/linecook/commands/share.rb
+++ lib/linecook/commands/share.rb
@@ -12,18 +12,42 @@ module Linecook
     # shares to be established.
     # 
     class Share < VboxCommand
-      config :name, 'vm'         # the share name
-      config :remote_dir, 'vm'   # the remote share dir
+      config :local_dir, 'vm'    # the local share dir
+      config :remote_dir, nil    # the remote share dir
       
       def sshfs_path
         @sshfs_path ||= `which sshfs`
       end
       
-      def sshfs?
+      def sshfs_installed?
         !sshfs_path.empty?
       end
       
+      def share(vm_name)
+        share_dir = File.expand_path(vm_name, local_dir)
+        FileUtils.mkdir_p(share_dir) unless File.exists?(share_dir)
+        
+        # a persistent socket appears necessary to prevent ssh from hanging
+        # after sshfs
+        start_ssh_socket(vm_name)
+        
+        sh! "sshfs -F '#{ssh_config_file}' '#{vm_name}:#{remote_dir}' '#{share_dir}'"
+      end
+      
+      def process(*vm_names)
+        unless sshfs_installed?
+          raise CommandError, "cannot share: sshfs is not installed"
+        end
+        
+        each_vm_name(vm_names) do |vm_name|
+          share(vm_name)
+        end
+      end
+      
+      private
+      
       REWRITE_FIELDS = %w{IdentityFile ControlPath}
+      
       def rewrite_ssh_configs(str)
         REWRITE_FIELDS.each do |field|
           str.gsub!(/^#{field}\s*(.*)$/) do |match|
@@ -34,30 +58,14 @@ module Linecook
         str
       end
       
-      def full_path_ssh_config_file
+      alias original_ssh_config_file ssh_config_file
+      
+      def ssh_config_file
         @full_path_ssh_config_file ||= begin
           @full_path_ssh_config_tempfile = Tempfile.new('full_path_ssh_config_file')
-          @full_path_ssh_config_tempfile << rewrite_ssh_configs(File.read(ssh_config_file))
+          @full_path_ssh_config_tempfile << rewrite_ssh_configs(File.read(original_ssh_config_file))
           @full_path_ssh_config_tempfile.close
           @full_path_ssh_config_tempfile.path
-        end
-      end
-      
-      def share_sshfs(vm_name, name, local_dir, remote_dir)
-        share_dir = "#{local_dir}/#{vm_name}"
-        FileUtils.mkdir_p(share_dir) unless File.exists?(share_dir)
-        sh! "sshfs -F '#{full_path_ssh_config_file}' '#{vm_name}:#{remote_dir}' '#{share_dir}'"
-      end
-      
-      def process(local_dir='vm', *vm_names)
-        local_dir = File.expand_path(local_dir)
-        
-        each_vm_name(vm_names) do |vm_name|
-          if sshfs?
-            share_sshfs(vm_name, name, local_dir, remote_dir)
-          else
-            share(vm_name, name, local_dir, remote_dir)
-          end
         end
       end
     end
