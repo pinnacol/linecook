1c28a90805abb8d823b5a79352a4a7efa9b5ffe9
Simon Chiang <simon.chiang@pinnacol.com>
2011-02-22 10:49:40 -0700

    changed default run remote_dir to pkg

--- lib/linecook/commands/run.rb
+++ lib/linecook/commands/run.rb
@@ -10,12 +10,12 @@ module Linecook
       SCP_SCRIPT = File.expand_path('../../../../bin/linecook_scp', __FILE__)
       
       config :project_dir, '.', :short => :d                 # the project directory
-      config :remote_dir, 'vm/pkg', :short => :D
-      config :ssh_config_file, 'config/ssh', :short => :F
+      config :remote_dir, 'pkg', :short => :D                # the remote package dir
+      config :ssh_config_file, 'config/ssh', :short => :F    # the ssh config file
       config :quiet, false, :short => :q, &c.flag            # silence output
-      config :transfer, true, &c.switch
+      config :transfer, true, &c.switch                      # transfer package (or not)
       config :file, false, &c.flag                           # treat package name as file path
-      config :runlist, nil
+      config :runlist, nil                                   # specify a runlist ('-' for stdnin)
       
       def process(*package_names)
         package_names = glob_package_names(project_dir) if package_names.empty?
