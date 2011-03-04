19298df5ed8657751c637286f9e7f2dc4f7d6653
Simon Chiang <simon.chiang@pinnacol.com>
2011-03-03 17:15:10 -0700

    allow printout of vbox state by host

--- lib/linecook/commands/state.rb
+++ lib/linecook/commands/state.rb
@@ -9,13 +9,22 @@ module Linecook
     # all virtual machines configured in config/ssh will print their state.
     #
     class State < VboxCommand
+      config :hosts, false, :short => :n, &c.flag   # print state by host
+      
       def state(vm_name)
         running?(vm_name) ? "running" : "stopped"
       end
       
       def process(*vm_names)
-        each_vm_name(vm_names) do |vm_name|
-          puts "#{vm_name}: #{state(vm_name)}"
+        if hosts
+          hosts = Hash[*load_hosts(ssh_config_file).flatten]
+          each_host(vm_names) do |host|
+            puts "#{host}: #{state(hosts[host])}"
+          end
+        else
+          each_vm_name(vm_names) do |vm_name|
+            puts "#{vm_name}: #{state(vm_name)}"
+          end
         end
       end
     end
