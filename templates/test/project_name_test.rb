334a8d03a0e9a1fb1d98a7008b8c073109caf77e
Simon Chiang <simon.chiang@pinnacol.com>
2011-03-09 10:53:33 -0700

    removed example helper test from template

--- templates/test/project_name_test.rb
+++ templates/test/project_name_test.rb
@@ -4,32 +4,6 @@ class <%= const_name %>Test < Test::Unit::TestCase
   include Linecook::Test
   
   #
-  # create_dir test
-  #
-  
-  def test_create_dir_creates_a_non_existant_dir
-    # assert_recipe builds the recipe in the block and checks that the
-    # result is as specified (note the result is outdented by default)
-    
-    assert_recipe(%{
-      if ! [ -d dir ]
-      then
-        echo "Create dir: dir"
-        mkdir -p dir
-      fi
-      
-    }){
-      helpers '<%= project_name %>'
-      create_dir 'dir'
-    }
-    
-    # now run all recipes you've created in the test
-    assert_output_equal %{
-      Create dir: dir
-    }, *run_package
-  end
-  
-  #
   # package test
   #
   
