60572a988f2823d97945e2b27b87ddbb68d5faf1
Simon Chiang <simon.chiang@pinnacol.com>
2011-07-30 23:09:25 -0600

    added/imported Package

---
+++ lib/linecook/package.rb
@@ -0,0 +1,89 @@
+module Linecook
+  class Package
+    # A string format used to determine the target_path for a given target
+    # name.  The remote_dir is intended to represent the directory where a
+    # package is ultimately deployed (and as such recipes can reliably
+    # reference target names). When remote_dir is set to nil the target_name
+    # is treated as the the target_path.
+    attr_accessor :remote_dir
+
+    # A registry of (target_name, source_path) pairs recording what files
+    # are included in the package.
+    attr_reader :registry
+
+    def initialize(remote_dir=nil)
+      @remote_dir = remote_dir
+      @registry   = {}
+    end
+
+    def resolve_source_path(source)
+      source_path = source.respond_to?(:path) ? source.path : source
+      unless source_path.kind_of?(String)
+        raise "could not resolve source to a path: #{source.inspect}"
+      end
+      File.expand_path(source_path)
+    end
+
+    # Registers the source to the target_name.
+    def add(target_name, source)
+      source_path = resolve_source_path(source)
+
+      if current = registry[target_name]
+        unless current == source_path
+          raise "already registered: #{target_name.inspect} (#{current.inspect})"
+        end
+      else
+        registry[target_name] = source_path
+      end
+
+      source
+    end
+
+    def rm(target_name)
+      registry.delete(target_name)
+    end
+
+    # Returns the source_path for target_name, as registered in self.  Returns
+    # nil if the target is not registered.
+    def source_path(target_name)
+      registry[target_name]
+    end
+
+    def target_path(target_name)
+      if registry.has_key?(target_name)
+        remote_dir ? (remote_dir % target_name) : target_name
+      else
+        nil
+      end
+    end
+
+    # Returns the content of the source_path for target_name, as registered in
+    # self.  Returns nil if the target is not registered.
+    def content(target_name, length=nil, offset=nil)
+      path = source_path(target_name)
+      path ? File.read(path, length, offset) : nil
+    end
+
+    def export(dir)
+      if File.exists?(dir)
+        raise "already exists: #{dir.inspect}"
+      end
+
+      registry.each_key do |target_name|
+        export_path = File.join(dir, target_name)
+        source_path = registry[target_name]
+
+        if source_path == export_path
+          next
+        end
+
+        export_dir = File.dirname(export_path)
+        FileUtils.mkdir_p(export_dir)
+        FileUtils.cp(source_path, export_path)
+        registry[target_name] = export_path
+      end
+
+      registry
+    end
+  end
+end
---
+++ test/linecook/package_test.rb
@@ -0,0 +1,78 @@
+require File.expand_path('../../test_helper', __FILE__)
+require 'linecook/package'
+require 'tempfile'
+
+class PackageTest < Test::Unit::TestCase
+  include ShellTest
+
+  Package = Linecook::Package
+
+  attr_accessor :package
+
+  def setup
+    super
+    @package = Package.new
+  end
+
+  #
+  # add test
+  #
+
+  def test_add_registers_source_file_to_target_name
+    package.add('target/path', 'source/path')
+    assert_equal File.expand_path('source/path'), package.registry['target/path']
+  end
+
+  def test_add_raises_error_for_target_name_added_to_a_different_source
+    package.add('target/path', 'source/a')
+
+    err = assert_raises(RuntimeError) { package.add('target/path', 'source/b') }
+    assert_equal %{already registered: "target/path" ("#{File.expand_path('source/a')}")}, err.message
+  end
+
+  def test_add_does_not_raise_error_for_double_add_of_same_source_and_target_name
+    package.add('target/path', 'source/a')
+    assert_nothing_raised { package.add('target/path', 'source/a') }
+  end
+
+  #
+  # content test
+  #
+
+  def test_content_returns_the_contents_of_the_target
+    source_path = prepare('source', 'content')
+    package.add 'target/path', source_path
+    assert_equal 'content', package.content('target/path')
+  end
+
+  def test_content_returns_the_specified_length_and_offset
+    source_path = prepare('source', 'content')
+    package.add 'target/path', source_path
+    assert_equal 'nte', package.content('target/path', 3, 2)
+  end
+
+  def test_content_returns_nil_for_unadded_target
+    assert_equal nil, package.content('not/added')
+  end
+
+  #
+  # export test
+  #
+
+  def test_export_copies_source_files_to_dir_as_specified_in_registry
+    original_source = prepare('example') {|io| io << 'content'}
+
+    package.registry['target/path'] = original_source
+    package.export path('export/dir')
+
+    assert_equal 'content', File.read(original_source)
+    assert_equal 'content', File.read(path('export/dir/target/path'))
+  end
+
+  def test_export_rewrites_and_returns_registry_with_new_source_paths
+    source_path = prepare('source', 'content')
+    package.add('target/path', source_path)
+    registry = package.export path('export/dir')
+    assert_equal path('export/dir/target/path'), registry['target/path']
+  end
+end
